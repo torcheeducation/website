d8c83cce90795ecb18b8e89aa7087efecfe377a7
Patricia Ho <hopatricia1711@gmail.com>
2022-08-05 16:34:51 +0700

    make awards page (layout only)

--- assets/js/main.js
+++ assets/js/main.js
@@ -367,7 +367,50 @@
         }
     }
 });
+
+/**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
   
+/**
+ * Porfolio isotope and filter
+ */
+let portfolionIsotope = document.querySelector('.portfolio-isotope');
+
+if (portfolionIsotope) {
+
+  let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
+  let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
+  let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
+
+  window.addEventListener('load', () => {
+    let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
+      itemSelector: '.portfolio-item',
+      layoutMode: portfolioLayout,
+      filter: portfolioFilter,
+      sortBy: portfolioSort
+    });
+
+    let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
+    menuFilters.forEach(function (el) {
+      el.addEventListener('click', function () {
+        document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
+        this.classList.add('filter-active');
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        if (typeof aos_init === 'function') {
+          aos_init();
+        }
+      }, false);
+    });
+
+  });
+
+}
 
   //auto-scroll for services pages
   // function autoScrollServicesPage() {
