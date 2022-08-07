49cae1c5d90686a522a74796a0e41b2c77deda9c
Patricia Ho <hopatricia1711@gmail.com>
2022-08-07 16:27:20 +0700

    migrating carousel from owl to bootstrap done, award page done, filter on progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -333,84 +333,42 @@
  */
   
   // Header Course carousel
-  $(".header-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1500,
-    items: 1,
-    dots: true,
-    loop: true,
-    nav : true,
-    navText : [
-        '<i class="bi bi-chevron-left"></i>',
-        '<i class="bi bi-chevron-right"></i>'
-    ]
-  });
+  // $(".header-carousel").owlCarousel({
+  //   autoplay: true,
+  //   smartSpeed: 1500,
+  //   items: 1,
+  //   dots: true,
+  //   loop: true,
+  //   nav : true,
+  //   navText : [
+  //       '<i class="bi bi-chevron-left"></i>',
+  //       '<i class="bi bi-chevron-right"></i>'
+  //   ]
+  // });
 
   // Testimonials carousel
-  $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    center: true,
-    margin: 24,
-    dots: true,
-    loop: true,
-    nav : false,
-    responsive: {
-        0:{
-            items:1
-        },
-        768:{
-            items:2
-        },
-        992:{
-            items:3
-        }
-    }
-});
-
-/**
-   * Initiate glightbox
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
+//   $(".testimonial-carousel").owlCarousel({
+//     autoplay: true,
+//     smartSpeed: 1000,
+//     center: true,
+//     margin: 24,
+//     dots: true,
+//     loop: true,
+//     nav : false,
+//     responsive: {
+//         0:{
+//             items:1
+//         },
+//         768:{
+//             items:2
+//         },
+//         992:{
+//             items:3
+//         }
+//     }
+// });
   
-/**
- * Porfolio isotope and filter
- */
-let portfolionIsotope = document.querySelector('.portfolio-isotope');
 
-if (portfolionIsotope) {
-
-  let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
-  let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
-  let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
-
-  window.addEventListener('load', () => {
-    let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
-      itemSelector: '.portfolio-item',
-      layoutMode: portfolioLayout,
-      filter: portfolioFilter,
-      sortBy: portfolioSort
-    });
-
-    let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
-    menuFilters.forEach(function (el) {
-      el.addEventListener('click', function () {
-        document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
-        this.classList.add('filter-active');
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        if (typeof aos_init === 'function') {
-          aos_init();
-        }
-      }, false);
-    });
-
-  });
-
-}
 
   //auto-scroll for services pages
   // function autoScrollServicesPage() {
@@ -426,3 +384,5 @@ if (portfolionIsotope) {
   // }
 
   //end auto-scroll for services pages
+
+  
